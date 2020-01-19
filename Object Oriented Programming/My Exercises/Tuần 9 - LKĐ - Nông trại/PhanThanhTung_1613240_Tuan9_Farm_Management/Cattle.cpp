85af9865ce59c72219133011c29efc481d6a6f7b
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 21:50:52 +0700

    Create Cattle.cpp

---
+++ Object Oriented Programming/My Exercises/Tuần 9 - LKĐ - Nông trại/PhanThanhTung_1613240_Tuan9_Farm_Management/Cattle.cpp
@@ -0,0 +1,35 @@
+#include "Cattle.h"
+
+Cattle::Cattle()
+{
+}
+
+
+Cattle::~Cattle()
+{
+}
+
+void Cattle::setSourceAmount()
+{
+	cin >> srcAmount;
+}
+
+int Cattle::getPregnant(int srcAmount)
+{
+	return 0;
+}
+
+float Cattle::getVmilk(int srcAmount)
+{
+	return 0.0f;
+}
+
+string Cattle::getBawl()
+{
+	return string();
+}
+
+int Cattle::getSrcAmount()
+{
+	return srcAmount;
+}
