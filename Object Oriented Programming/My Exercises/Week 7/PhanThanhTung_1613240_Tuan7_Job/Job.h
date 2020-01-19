f499cd424c1d67539fac6396154f1a76ee7c36d1
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 21:53:11 +0700

    Create Job.h

---
+++ Object Oriented Programming/My Exercises/Week 7/PhanThanhTung_1613240_Tuan7_Job/Job.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+using namespace std;
+class Job
+{
+protected:
+	string Name;
+	int YearofBirth;
+	string Address;
+public:
+	Job();
+	~Job();
+	void SetInfor();
+	string getName();
+	int getYoB();
+	string getAddress();
+	void Output();
+};
+
