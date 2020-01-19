4b21c222f502f892981bd36ef6a05e3041afc8b0
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 21:54:19 +0700

    Create Cow.cpp

---
+++ Object Oriented Programming/My Exercises/Week 8/PhanThanhTung_1613240_Tuan8_FARM/Cow.cpp
@@ -0,0 +1,27 @@
+#include "Cow.h"
+#include "time.h"
+
+Cow::Cow()
+{
+}
+
+Cow::~Cow()
+{
+}
+
+int Cow::getBaby(int iCow)
+{
+	srand((unsigned)time(0));
+	return iPregnant = iCow * (rand()%10 +1);
+}
+
+float Cow::getVmilk(int iCow)
+{
+	srand((unsigned)time(0));
+	return Vmilk = iCow * (rand()%20);
+}
+
+string Cow::getBawl()
+{
+	return Bawl = "Umboar Umboar Umboar ...";
+}
